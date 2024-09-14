082abe2acceb1c45631b66a89208789cea59b4c2
kriNon1 <3295424+kriNon@users.noreply.github.com>
2024-09-14 10:02:59 +1000

    Disabling USE_LONG_TAILED_CHROMA_ATTENUATION
    Fixes some issues in dark scenes

--- Source/AdvancedTonemapping/Private/AdvancedTonemapping.cpp
+++ Source/AdvancedTonemapping/Private/AdvancedTonemapping.cpp
@@ -13,7 +13,7 @@ void FAdvancedTonemappingModule::StartupModule()
 	UnrealShaderModLoader::AddModification_Comment(TEXT("PostProcessCombineLUTs.usf"), R"(ColorAP1 = lerp( ColorAP1, mul( BlueCorrectAP1, ColorAP1 ), BlueCorrection );)");
 	UnrealShaderModLoader::AddModification_Comment(TEXT("PostProcessCombineLUTs.usf"), R"(ColorAP1 = lerp( ColorAP1, mul( BlueCorrectInvAP1, ColorAP1 ), BlueCorrection );)");
 	UnrealShaderModLoader::AddModification_Replace(TEXT("PostProcessCombineLUTs.usf"),R"(float3 ToneMappedColorAP1 = FilmToneMap( ColorAP1 );)", R"(float3 ToneMappedColorAP1 = display_transform_AP1( ColorAP1 );)");
-	
+
 	USMLFileHandler::CopyShaderOverridesToUnrealShaderModLoader(TEXT("AdvancedTonemapping"));
 }
 
